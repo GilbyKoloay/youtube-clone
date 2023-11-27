20162ba441a9b4cee8375a68d540c75100c3c6d4
GilbyKoloay <lolwtf10001@gmail.com>
2023-11-28 02:35:45 +0800

    modified toProperCount function to stringify value

--- src/functions/toProperCount.js
+++ src/functions/toProperCount.js
@@ -1,7 +1,9 @@
 export default function toProperCount(value) {
-  if (value.toString().length > 9) return `${value.slice(0, 3)}.${value[3]}B`;
-  if (value.toString().length > 6) return `${value.slice(0, 3)}.${value[3]}M`;
-  if (value.toString().length > 3) return `${value.slice(0, 3)}K`;
+  const properedValue = value.toString();
+
+  if (properedValue.length > 9) return `${properedValue.slice(0, 3)}.${properedValue[3]}B`;
+  if (properedValue.length > 6) return `${properedValue.slice(0, 3)}.${properedValue[3]}M`;
+  if (properedValue.length > 3) return `${properedValue.slice(0, 3)}K`;
   
-  return value.toString();
+  return properedValue;
 };
